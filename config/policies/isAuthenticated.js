c24667da4aa14fbf2881b8a35bfc5b7d8fcb8939
Sergio Valencia <serg337@gmail.com>
2019-12-27 00:58:43 +0100

    installed graphql plugin

--- config/policies/isAuthenticated.js
+++ config/policies/isAuthenticated.js
@@ -4,7 +4,7 @@ module.exports = async (ctx, next) => {
     // retrieve the user id
     const { id } = ctx.state.user;
     // the request is for adding a new track POST /tracks
-    // and the key `creator` is the relation
+    // and the key `userId` is the relation
     ctx.request.body.userId = id;
     // continue the middleware stack (will call the controller action to create an article)
     return await next();
