e0d5b5de71559f896d6cb7cb9295146e07a12bec
Sergio Valencia <serg337@gmail.com>
2019-12-27 10:45:46 +0100

    Implemented GET /tracks controller override to filter by userId

--- api/track/controllers/track.js
+++ api/track/controllers/track.js
@@ -1,8 +1,38 @@
 "use strict";
-
+const { sanitizeEntity } = require("strapi-utils");
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
-
-module.exports = {};
+module.exports = {
+  /**
+   * Retrieve records.
+   *
+   * @return {Array}
+   */
+  async find(ctx) {
+    let entities;
+    let query = ctx.query;
+    if (!ctx.state.user) {
+      ctx.status = 403;
+      return {
+        error: "unauthorized",
+        message: "Your user doesn't exist or you are not logged in"
+      };
+    } else {
+      if (!query.userId) {
+        query.userId = ctx.state.user.id;
+      } else if (query.userId !== ctx.state.user.id) {
+        query.userId = ctx.state.user.id;
+      }
+    }
+    if (query._q) {
+      entities = await strapi.services.track.search(query);
+    } else {
+      entities = await strapi.services.track.find(query);
+    }
+    return entities.map(entity =>
+      sanitizeEntity(entity, { model: strapi.models.track })
+    );
+  }
+};
